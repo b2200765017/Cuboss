88e5f2070e146d9761107aa2bd330ac2ff25173a
Berkan Yunus Marancı <59084840+b2200356038@users.noreply.github.com>
2023-02-01 02:37:13 +0300

    d
    d

---
+++ Assets/destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+    [SerializeField] private int time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject,time);
+    }
+}
